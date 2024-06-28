e47262c4ca83cc7e62c284d0e6c79857ce06ca65
Masum <h.m.asrafulmasum@gmail.com>
2024-06-29 03:39:26 +0600

    projects page working on

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,45 +2,16 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>
-      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+    <div className='bg-white py-28'>
+      <ul className="timeline max-md:timeline-compact timeline-vertical max-w-screen-xl mx-4 md:mx-10 xl:mx-auto">
         <li>
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5">
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd" />
-            </svg>
-          </div>
-          <div className="timeline-start mb-10 md:text-end">
-            <time className="font-mono italic">1984</time>
-            <div className="text-lg font-black">First Macintosh computer</div>
-            The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
-            personal computer. It played a pivotal role in establishing desktop publishing as a general
-            office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed
-            in a beige case with integrated carrying handle; it came with a keyboard and single-button
-            mouse.
+          
+          <div className="timeline-start w-full">
+            <h2 className='text-7xl font-bold'>My recent <br /> <span className='text-primary italic text-9xl'>works</span></h2>
           </div>
         </li>
         <li>
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5">
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd" />
-            </svg>
-          </div>
-          <div className="timeline-end mb-10">
+          <div className="timeline-end">
             <time className="font-mono italic">1998</time>
             <div className="text-lg font-black">iMac</div>
             iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has
@@ -49,19 +20,7 @@ function Projects() {
           </div>
         </li>
         <li>
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5">
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd" />
-            </svg>
-          </div>
-          <div className="timeline-start mb-10 md:text-end">
+          <div className="timeline-start md:text-end">
             <time className="font-mono italic">2001</time>
             <div className="text-lg font-black">iPod</div>
             The iPod is a discontinued series of portable media players and multi-purpose mobile devices
@@ -72,19 +31,7 @@ function Projects() {
           </div>
         </li>
         <li>
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5">
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd" />
-            </svg>
-          </div>
-          <div className="timeline-end mb-10">
+          <div className="timeline-end">
             <time className="font-mono italic">2007</time>
             <div className="text-lg font-black">iPhone</div>
             iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile
@@ -95,19 +42,7 @@ function Projects() {
           </div>
         </li>
         <li>
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5">
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd" />
-            </svg>
-          </div>
-          <div className="timeline-start mb-10 md:text-end">
+          <div className="timeline-start md:text-end">
             <time className="font-mono italic">2015</time>
             <div className="text-lg font-black">Apple Watch</div>
             The Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness
